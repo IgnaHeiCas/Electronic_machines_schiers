fc1b61edb0976a3cd8e81a68510d979ec9cb3808
IgnaHeiCas <ignacioheinigercascos@gmail.com>
2026-05-08 17:56:11 +0200

    Fahren.py
    Einen Test für axle track und wheel diameter hinzugefügt

--- Test_Phase/Funktional/Fahren.py
+++ Test_Phase/Funktional/Fahren.py
@@ -4,12 +4,14 @@ from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
 hub = PrimeHub()
+
 left_motor = Motor(Port.A, profile=11, positive_direction=Direction.COUNTERCLOCKWISE)
 right_motor = Motor(Port.B)
 
 #Gyro test
 drive = DriveBase(right_motor, left_motor, wheel_diameter=50, axle_track=75)
 
+#Test für genauigkeit bei Track und Diameter
 #print(motorA.angle())
 #left_motor.run_angle(-1000, 1000, wait=False)
 #right_motor.run_angle(1000, 1000)
