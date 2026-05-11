356cbe6915f335f38fdc2ef0683a346b9bf0952f
levcot <cottilevin@gmail.com>
2026-05-11 16:17:25 +0200

    Update Main.py

--- WRO_2026/WRO 2026 Pybricks/Main.py
+++ WRO_2026/WRO 2026 Pybricks/Main.py
@@ -7,43 +7,44 @@ from pybricks.tools import wait, StopWatch, hub_menu, run_task, multitask
 hub = PrimeHub()
 # === Setup ===
 #Fahren
-l_motor = Motor(Port.A, profile=4, positive_direction=Direction.COUNTERCLOCKWISE)
-r_motor = Motor(Port.B, profile=4)
+l_motor = Motor(Port.A, positive_direction=Direction.COUNTERCLOCKWISE, profile=5)
+r_motor = Motor(Port.B, profile=5)
 #Arme
-hoch_arm = Motor(Port.E, profile=4)
-zangen_arm = Motor(Port.F, profile=4, gears=[12, 20])
+hoch_arm = Motor(Port.F)
+zangen_arm = Motor(Port.D, gears=[12, 20])
 
-drive = DriveBase(r_motor, l_motor, wheel_diameter=50, axle_track=75)
+drive = DriveBase(r_motor, l_motor, wheel_diameter=62.5, axle_track=196.5)
 
-drive.settings(straight_speed=800, straight_acceleration=1000, turn_rate=800, turn_acceleration=500)
+drive.settings(straight_speed=800, straight_acceleration=1000, turn_rate=500, turn_acceleration=500)
 
-async def Kalibrieren(): #async ist für await und multitask
+def hoch(): #async ist für await und multitask
     #Kalibriert den Roboter Arm auf physische Limit
-    await multitask(
-        hoch_arm.run_until_stalled(-300, duty_limit=50),
-        zangen_arm.run_until_stalled(-300, duty_limit=50)
-    )
+    hoch_arm.run_until_stalled(-800, duty_limit=100),
     hoch_arm.reset_angle(0)
+
+def zange_hoch(): 
+
+    #Kalibriert den Roboter Arm auf physische Limit
+    zangen_arm.run_until_stalled(-800, duty_limit=100),
     zangen_arm.reset_angle(0)
 
-def halte_blöcke(laufen):
-    if laufen:
+def halte_blöcke():
+    if True:
         zangen_arm.run(200)
     else:
         zangen_arm.brake()
 
-
 # === Programme ===
 def gelbe_bausteine():
-    drive.settings(straight_speed=800, straight_acceleration=100, turn_rate=800, turn_acceleration=100)
+    hoch()
     drive.straight(350)
     drive.straight(-50)
     zangen_arm.track_target(190)
     hoch_arm.run_until_stalled(800)
     halte_blöcke(True)
-    Kalibrieren()
 
 def aus_gelbe_steine_fahren():
+    hoch()
     drive.straight(-235)
     drive.turn(90)
     drive.straight(-250)
@@ -75,8 +76,10 @@ def bauklotz_1():
     halte_blöcke(False)
 
 # === hier laufen lassen ===
-Kalibrieren()
-gelbe_bausteine()
-aus_gelbe_steine_fahren()
-bauklotz_1()
+#zangen_arm.run_until_stalled(-800)
+#zangen_arm.reset_angle(0)
+
+halte_blöcke(True)
+
+
 
